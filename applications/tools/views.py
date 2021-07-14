9505e1069ab1a133c5211566c0df859c56106384
Jorge Infante <56237414+Jorge-Infante@users.noreply.github.com>
2021-07-13 22:22:58 -0500

    dewarpTools ftp download

--- applications/tools/views.py
+++ applications/tools/views.py
@@ -12,7 +12,9 @@ from django.template.loader import render_to_string
 #Funciones
 from . dewarp import ejecutar 
 
-
+import shutil
+import urllib.request as request2
+from contextlib import closing
 
 
 
@@ -21,29 +23,35 @@ def correr(request):
     # url_link = "https://ingenioitc.com/mipg/34.mp4"
     # urllib.request.urlretrieve(url_link, 'static/in/1.mp4') 
     # ejecutar()
+    HOSTNAME = "ftp.ingenioitc.com"
+    USERNAME = "ftpdocker@docker.ingenioitc.com"
+    PASSWORD = "c4nd4d0$"
+
+    ftp = ftplib.FTP(HOSTNAME)
+    ftp.login(USERNAME,PASSWORD)
+    ftp.set_pasv(False)  
+    remotefile='/in/entra1.mp4'
+    download='static/in/entrada1.mp4'
+    with open(download, 'wb') as file:
+        ftp.retrbinary('RETR %s' %remotefile, file.write )
+
     
 
-    try:
-        passwd ='c4nd4d0$'
-        if not passwd:
-            quit()
-        try:
-            s = ftplib.FTP('ftp.ingenioitc.com','ftpdocker@docker.ingenioitc.com', passwd)
-            f = open('static/in/1.mp4', 'rb')
-            s.storbinary('STOR https://docker.ingenioitc.com/out/1.mp4', f)
- 
-            f.close()
-            s.quit()
-            print (": )")
-        except:
-            quit()
- 
-    except:
-        quit()
-  
+    # with closing(request2.urlopen('ftp://tpdocker@docker.ingenioitc.com:c4nd4d0$@ftp.ingenioitc.com/in/', 'file')) as r:
+    #     with open('file', 'wb') as f:
+    #         shutil.copyfileobj(r, f)
     return HttpResponse('<h1>Video renderizado correctamente </h1>')
 
+def out(request):
 
+    HOSTNAME = "ftp.ingenioitc.com"
+    USERNAME = "ftpdocker@docker.ingenioitc.com"
+    PASSWORD = "c4nd4d0$"
+
+    
+
+
+    return HttpResponse('<h1>Video subido correctamente</h1>')
 
 
 
